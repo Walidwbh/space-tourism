1579af838cf63bf7c3d023ca63787f870bf2bb24
walidwbh <93353066+Walidwbh@users.noreply.github.com>
2023-04-25 18:30:05 +0100

    destination page

--- js/script.js
+++ js/script.js
@@ -5,5 +5,22 @@ $(document).ready(
             $(".hamburger").toggleClass("active");
             $(".navbar").toggleClass("active");
         })
+        //active class for ham menu
+        $(".navbar li").each(function(){
+            if($(this).data("class") == $("body").attr("class")){
+                $(this).addClass("active")
+            }            
+        })
+        //destination tabs
+        $(".tab-content").hide();
+        $(".tab-content:first").show();
+        $(".tabs li:first").addClass("active");
+        $(".tabs li").click(function(event){
+            $(".tabs li").removeClass("active");
+            $(this).addClass("active")
+            $(".tab-content").hide();
+            var selectedTab = $(this).find('a').attr("href");
+            $(selectedTab).fadeIn();
+        })
     }
 )
